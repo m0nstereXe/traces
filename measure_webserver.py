7222421424b44181f154a4ccf0ce73c080733548
Nick <nickbelov2003@gmail.com>
2023-12-14 13:21:44 -0500

    moar init

---
+++ measure_webserver.py
@@ -0,0 +1,16 @@
+from scapy.all import *
+import sys
+import time
+import math 
+
+
+
+
+
+def main():
+    input_file = sys.argv[1]
+    ip,port = sys.argv[2],int(sys.argv[3])
+    pass
+
+if __name__ == '__main__':
+    main()
